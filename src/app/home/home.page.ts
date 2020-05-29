85465541bdbf10e6f4378f08177617a9fde9a109
RuanChistopher <ruancristopherlc@gmail.com>
2020-05-28 21:43:20 -0300

    tudo ok

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,12 +11,19 @@ declare var google: any;
 export class HomePage {
   map: any;
   posicaoAtual: any;
+  local1: any;
+  local2: any;
+  local3: any;
+  local4: any;
+  local5: any;
+
   @ViewChild('map', {read: ElementRef, static: false}) mapRaf: ElementRef; 
 
   constructor(private geolocation: Geolocation)  {}
 
   public async showMap(){
     //const location = new google.maps.LatLng(-22.489130, -48.546436);
+  
     await this.buscaPosicao();
     const options={
       center: this.posicaoAtual,
@@ -25,12 +32,56 @@ export class HomePage {
     }
     this.map == new google.maps.Map(this.mapRaf.nativeElement, options)
     const marcador = new google.maps.Manker({
-      position: this.posicaoAtual,
+      position: this.posicaoAtual, 
       map: this.map,
-      tittle: "",
-      icon: 'htpps://maps.google.com/mapfiles/ms/icons/green-dot.png',
-      animation: google.maps.Animation.
+      tittle: "Esta aqui vós merce",
+      icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
+      animation: google.maps.Animation.DROP
     });
+
+    const local1 = new google.maps.local1({
+        position: new google.maps.LatLng(-22.495713, -48.551065),
+        map: this.map,
+        title: "Aqui tem Lanche foda",
+        //colocar ícones
+        icon: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
+        animation: google.maps.Animation.BOUNCE
+    });
+    const local2 = new google.maps.local2({
+        position: new google.maps.LatLng(-22.499467, -48.555141),
+        map: this.map,
+        title: "Cabeleireiro foda",
+        //colocar ícones
+        icon: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
+        animation: google.maps.Animation.BOUNCE
+    });
+    const local3 = new google.maps.local3({
+        position: new google.maps.LatLng(-22.499327, -48.563401),
+        map: this.map,
+        title: "Não pode vim aqui",
+        //colocar ícones
+        icon: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
+        animation: google.maps.Animation.BOUNCE
+    });
+
+    const local4 = new google.maps.local4({
+        position: new google.maps.LatLng(-22.495730, -48.566072),
+        map: this.map,
+        title: "Se estiver aqui não é um bm sinal",
+        //colocar ícones
+        icon: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
+        animation: google.maps.Animation.BOUNCE
+    });
+    const local5 = new google.maps.local5({
+        position: new google.maps.LatLng(-22.494514, -48.558450),
+        map: this.map,
+        title: "Boom de mais o lanche daqui, só cuidado ao pedir ao ponto, as vezes vem mal passad",
+        //colocar ícones
+        icon: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
+        animation: google.maps.Animation.BOUNCE
+    });
+
+
   }
   ionViewDidEnter(){
     this.showMap();
